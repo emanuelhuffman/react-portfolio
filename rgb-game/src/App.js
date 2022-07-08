f946427f468cda5d15cf22183565a000d4dea074
Emanuel Huffman <ehvdb@mst.edu>
2022-07-07 22:37:12 -0400

    components created

---
+++ rgb-game/src/App.js
@@ -0,0 +1,16 @@
+import Display from "./components/Display";
+import Nav from "./components/Nav";
+import Grid from "./components/Grid";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Display />
+      <Nav />
+      <Grid />
+    </div>
+  );
+}
+
+export default App;
